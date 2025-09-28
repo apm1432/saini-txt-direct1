872e77d8b27f8660414bcb65493601b73f2498cf
AnataVortex7 <akshaymerat@gmail.com>
2025-09-28 21:33:22 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -11,7 +11,7 @@ OWNER = int(environ.get("OWNER", "1193564058"))
 CREDIT = environ.get("CREDIT", "𝕒𝕜𝕤𝕙𝕒𝕪")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '1193564058,7268596608').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
 AUTH_USER = os.environ.get('AUTH_USERS', '1193564058,7268596608').split(',')
@@ -23,5 +23,6 @@ if int(OWNER) not in AUTH_USERS:
 api_url = "http://master-api-v3.vercel.app/"
 api_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNzkxOTMzNDE5NSIsInRnX3VzZXJuYW1lIjoi4p61IFtvZmZsaW5lXSIsImlhdCI6MTczODY5MjA3N30.SXzZ1MZcvMp5sGESj0hBKSghhxJ3k1GTWoBUbivUe1I"
 # .....,.....,.......,...,.......,....., .....,.....,.......,...,.
+
 
 
